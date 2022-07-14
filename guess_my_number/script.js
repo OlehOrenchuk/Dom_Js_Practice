00d4e2465bc736c9ff1bad86d1325a27db523362
OlehOrenchuk <Oleh.Orenchuk.KNM.2019@lpnu.ua>
2022-07-14 03:38:26 +0300

    Fixed bug with zero number + add reset boundaries

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -8,27 +8,29 @@ function generateRandom(min = +(document.querySelector('.first_boundary').value)
     return rand
   }
   else {
-    // console.log(`Рекурсія з числом ${rand}`)
-    generateRandom(min, max)
+    return generateRandom()
   }
 }
-
 
 document.querySelector('.confirm').addEventListener('click', function(){
   const first_boundary = (document.querySelector('.first_boundary').value)
   const second_boundary = (document.querySelector('.second_boundary').value)
-  document.querySelector('nav').style.visibility = 'hidden'
-  document.querySelector('.between').textContent = `(Between ${first_boundary} and ${second_boundary})`
-  document.body.style.backgroundColor = '#222'
-  document.querySelector('.again').style.visibility = 'visible'
-  document.querySelector('main').style.visibility = 'visible'
-  globalThis.value = generateRandom()
-  // console.log(value)
+  if(first_boundary && second_boundary) {
+    document.querySelector('nav').style.visibility = 'hidden'
+    document.querySelector('.between').textContent = `(Between ${first_boundary} and ${second_boundary})`
+    document.body.style.backgroundColor = '#222'
+    document.querySelector('.again').style.visibility = 'visible'
+    document.querySelector('main').style.visibility = 'visible'
+    document.querySelector('.reset').style.visibility = 'visible'
+    globalThis.value = generateRandom()
+  }
+  else {
+    alert("Error! Enter first and second boundaries!")
+  }
 })
 
 document.querySelector('.check').addEventListener('click', function(){
   const guess = +(document.querySelector('.guess').value)
-
   if(!guess) {
     document.querySelector('.message').textContent = 'Enter not null number!'
   }
@@ -71,12 +73,19 @@ document.querySelector('.check').addEventListener('click', function(){
 
 document.querySelector('.again').addEventListener('click', function(){
   globalThis.value = generateRandom()
-  // console.log(value)
   document.body.style.backgroundColor = '#222'
   document.querySelector('.number').textContent = "?"
   document.querySelector('.score').textContent = '20'
   document.querySelector('main').style.visibility = 'visible'
   document.querySelector('.message').textContent = 'Start guessing...'
   document.querySelector('h1').textContent = 'Guess My Number!'
-  document.querySelector('.help_main').remove()
+
+  if(document.querySelector('.help_main')) {
+    document.querySelector('.help_main').remove()
+  }
+
+})
+
+document.querySelector('.reset').addEventListener('click', function(){
+  window.location.reload();
 })
